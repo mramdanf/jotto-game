a6a62d4a0f000f6e6ca171f5b3f1855959749535
Ramdan <ramdan.firdaus@metrasat.co.id>
2019-04-07 06:06:45 +0700

    Chapter 3 vid 27 - Set up jotto and Congrats component

---
+++ src/Congrats.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @returns {JSX.Element} - Rendered componet (or null if `success` props is false)
+ */
+export default () => {
+  return (
+    <div></div>
+  )
+}
