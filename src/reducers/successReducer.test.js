75fd14edc2f8360b25fb55def01e59bb97c56bbd
Ramdan <ramdan.firdaus@metrasat.co.id>
2019-04-07 20:07:07 +0700

    Chapter 5 vid 52 - 'successReducer' Code

--- src/reducers/successReducer.test.js
+++ src/reducers/successReducer.test.js
@@ -2,7 +2,7 @@ import { actionTypes } from '../actions'
 import successReducer from './successReducer'
 
 test('return default initial state of `false` when no action is passed', () => {
-  const newState = successReducer()
+  const newState = successReducer(undefined, {})
   expect(newState).toBe(false)
 })
 test('returns state of true upon receiving an action of type `CORRECT_ACTION`', () => {
