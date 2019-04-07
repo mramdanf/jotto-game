ebbe402f579aef79d42db1db03d0a84b7d51eb67
Ramdan <ramdan.firdaus@metrasat.co.id>
2019-04-07 20:31:55 +0700

    Chapter 5 vid 56 - Input Component Test Organization

--- src/Input.test.js
+++ src/Input.test.js
@@ -4,4 +4,18 @@ import { shallow } from 'enzyme'
 import { findByTestAttr } from '../test/testUtils'
 import Input from './Input'
 
-test('', () => {})
+describe('renders', () => {
+  describe('word has not been guessed', () => {
+    test('renders component without error', () => {})
+    test('renders input box', () => {})
+    test('renders submit button', () => {})
+  })
+  describe('word has been guessed', () => {
+    test('renders component without error', () => {})
+    test('does not render input box', () => {})
+    test('does not render submit button', () => {})
+  })
+})
+describe('update state', () => {
+
+})
