ee083e6ac4ca0233136c572d330b21044623e5c6
Ramdan <ramdan.firdaus@metrasat.co.id>
2019-04-07 19:05:27 +0700

    Chapter 5 vid 49 - Setup redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words,
+ * @param {object} action - Action to be reduced
+ * @returns {boolean} - New success state
+ */
+export default (state, action) => {
+  return null
+}
