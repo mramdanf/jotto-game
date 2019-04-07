f990a59e9ac63a16d9a4268e179f913594f093f9
Ramdan <ramdan.firdaus@metrasat.co.id>
2019-04-07 13:33:48 +0700

    Chapter 3 vid 33 - Default props in setup()

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -7,6 +7,8 @@ import Congrats from './Congrats'
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
+const defaultProps = { success: false }
+
 /**
  * Factory function to create a ShallowWrapper for the Congrats component.
  * @function setup
@@ -14,11 +16,12 @@ Enzyme.configure({ adapter: new EnzymeAdapter() })
  * @returns {ShallowWrapper}
  */
 const setup = (props={}) => {
-  return shallow(<Congrats {...props} />)
+  const setupProps = { ...defaultProps, ...props }
+  return shallow(<Congrats {...setupProps} />)
 }
 
 test('renders whitout error', () => {
-  const wrapper = setup({ success: false })
+  const wrapper = setup()
   const componentCongrats = findByTestAttr(wrapper, 'component-congrats')
   expect(componentCongrats.length).toBe(1)
 })
