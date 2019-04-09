f909f56e3f60ef893c317a83e28738148a8bc40a
Ramdan <ramdan.firdaus@metrasat.co.id>
2019-04-09 14:21:24 +0700

    Chapter 6 vid 71 - First Thunk Test

--- src/integration.test.js
+++ src/integration.test.js
@@ -2,8 +2,27 @@ import { storeFactory } from '../test/testUtils'
 import { guessWord } from './actions'
 
 describe('guessWord action dispatcher', () => {
+  const secretWord = 'party'
+  const unsuccessfullGuess = 'train'
   describe('no guessed words', () => {
-    test('update state correctly for unsuccessfull guess', () => {})
+    let store
+    const initialState = { secretWord }
+    beforeEach(() => {
+      store = storeFactory(initialState)
+    })
+    test('update state correctly for unsuccessfull guess', () => {
+      store.dispatch(guessWord(unsuccessfullGuess))
+      const newState = store.getState()
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [{
+          guessedWord: unsuccessfullGuess,
+          letterMatchCount: 3
+        }]
+      }
+      expect(newState).toEqual(expectedState)
+    })
     test('update state correctly for successfull guess', () => {})
   })
   describe('some guessed words', () => {
