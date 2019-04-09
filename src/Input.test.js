67f8cb2abc378f217a4659997766a9c7fe01b589
Ramdan <ramdan.firdaus@metrasat.co.id>
2019-04-09 11:22:55 +0700

    Chapter 5 vid 61 - Input Test and Code for success=false

--- src/Input.test.js
+++ src/Input.test.js
@@ -14,17 +14,28 @@ import UnconnectedInput from './UnconnectedInput'
  */
 const setup = (initialState={}) => {
   const store = storeFactory(initialState)
-  const wrapper = shallow(<Input store={store} />).dive()
-  console.log(wrapper.debug())
+  return shallow(<Input store={store} />).dive()
 }
-
-setup()
 
 describe('renders', () => {
   describe('word has not been guessed', () => {
-    test('renders component without error', () => {})
-    test('renders input box', () => {})
-    test('renders submit button', () => {})
+    let wrapper
+    beforeEach(() => {
+      const initialState = { success: false }
+      wrapper = setup(initialState)
+    })
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, 'component-input')
+      expect(component.length).toBe(1)
+    })
+    test('renders input box', () => {
+      const inputBox = findByTestAttr(wrapper, 'input-box')
+      expect(inputBox.length).toBe(1)
+    })
+    test('renders submit button', () => {
+      const submitButton = findByTestAttr(wrapper, 'submit-button')
+      expect(submitButton.length).toBe(1)
+    })
   })
   describe('word has been guessed', () => {
     test('renders component without error', () => {})
