9643ae37972332d6b42b4d126f238423f1519c6e
Ramdan <ramdan.firdaus@metrasat.co.id>
2019-04-09 21:25:57 +0700

    Chapter 8 vid 86 - Quiz! App Redux Props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
+import './App.css'
 import GuessedWords from './GuessedWords'
 import Congrats from './Congrats'
+import Input from './Input'
+import { getSecretWord } from './actions'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}/ >
+        <Congrats success={this.props.success} />
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ success, secretWord, guessedWords }) => {
+  return {
+    success,
+    secretWord,
+    guessedWords
+  }
+}
+
+export default connect(mapStateToProps, { getSecretWord })(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import App from './App'
+import { storeFactory } from '../test/testUtils'
+
+const setup = (initialState={}) => {
+  const store = storeFactory(initialState)
+  const wrapper = shallow(<App store={store} />).dive()
+  return wrapper
+}
+
+describe('redux props', () => {
+ 
+  test('has success piece of state in the props', () => {
+    const success = true
+    const wrapper = setup({ success })
+    const successProps = wrapper.instance().props.success
+    expect(successProps).toBe(success)
+  })
+ 
+  test('has secretWord piece of state in the props', () => {
+    const secretWord = 'party'
+    const wrapper = setup({ secretWord })
+    const secretWordProp = wrapper.instance().props.secretWord
+    expect(secretWordProp).toBe(secretWord)
+  })
+ 
+  test('has guessedWords piece of state in the props', () => {
+    const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }]
+    const wrapper = setup({ guessedWords })
+    const guessedWordsProps = wrapper.instance().props.guessedWords
+    expect(guessedWordsProps).toEqual(guessedWords)
+  })
+ 
+  test('`getSecretWord` action creator is a function props', () => {
+    const wrapper = setup()
+    const getSecretWordProp = wrapper.instance().props.getSecretWord
+    expect(getSecretWordProp).toBeInstanceOf(Function)
+  })
+})
