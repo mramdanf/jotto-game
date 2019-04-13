1322802abae2c9c9be84ff94e320b347551115a1
Ramdan <ramdan.firdaus@metrasat.co.id>
2019-04-13 16:16:15 +0700

    Challenge 2 Fix

--- src/App.js
+++ src/App.js
@@ -19,9 +19,12 @@ export class UnconnectedApp extends Component {
         <h1>Jotto</h1>
         <div>The secret word is {this.props.secretWord}</div>
         <Congrats success={this.props.success} />
+        <NewWordButton 
+          display={this.props.success} 
+          resetAction={this.props.resetGame} 
+        />
         <Input />
         <GuessedWords guessedWords={this.props.guessedWords} />
-        <NewWordButton display={this.props.success} resetAction={this.props.resetGame} />
         <TotalGuessedWords 
           data-test="component-total-guessed-words"
           totalGuessedWords={this.props.guessedWords.length} 
--- src/actions/index.js
+++ src/actions/index.js
@@ -32,16 +32,23 @@ export const guessWord = (guessedWord) => {
   }
 }
 
-export const getSecretWord = () => {
-  return (dispatch) => {
-    return axios.get('http://localhost:3030')
-      .then(response => {
-        dispatch({
-          type: actionTypes.SET_SECRET_WORD,
-          payload: response.data
-        })
+const getSecretWordDispatch = (dispatch) => {
+  return axios.get('http://localhost:3030')
+    .then(response => {
+      dispatch({
+        type: actionTypes.SET_SECRET_WORD,
+        payload: response.data
       })
-  }
+    })
 }
 
-export const resetGame = () => {}
+export const getSecretWord = () => {
+  return getSecretWordDispatch
+}
+
+export const resetGame = () => {
+  return (dispatch) => {
+    dispatch({ type: actionTypes.RESET_GAME })
+    return getSecretWordDispatch(dispatch)
+  }
+}
